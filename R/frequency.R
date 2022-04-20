82fec6216450c962d3fb0a976ecb3258230e8b30
agkamel <alexandre.gellen.kamel@gmail.com>
2022-04-20 17:54:58 -0400

    Initial commit

---
+++ R/frequency.R
@@ -0,0 +1,24 @@
+#' @title Compute frequency
+#'
+#' @param count Integer. The number of response.
+#' @param time Numeric. The number of minute.
+#' @param rule Rule to apply when the number of response is zero. The Neely rule is applied by default.
+#'
+#' @return A numeric vector.
+#'
+#' @examples #pt_frequency()
+#' @export
+pt_frequency <- function(count, time, rule = log10(2)) {
+  # Fonction pt_frequency
+  # Note: bloquer celui de stats::frequency si le nom n'est pas pt_frequency
+  dplyr::case_when(
+    count == 0 ~ .frequency_if_zero_count(time, rule = rule),
+    TRUE       ~ count / time
+  )
+}
+
+.frequency_if_zero_count <- function(time, rule = log10(2)){
+  # Fonction helper .frequency_if_zero_count ####
+  #    Selon la suggestion de l'article de Neely
+  antilog(log10(pt_time_floor(time)) - rule)
+}
