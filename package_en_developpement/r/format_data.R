f675eda5c903e509de5a34654617e2c541830fac
agkamel <alexandre.gellen.kamel@gmail.com>
2022-04-23 00:57:34 -0400

    Modification

--- package_en_developpement/r/format_data.R
+++ package_en_developpement/r/format_data.R
@@ -2,17 +2,35 @@
 library(dplyr)
 library(lubridate)
 
-# Function
-format_data <- function(data, date, count, time, date_zero) {
-  data_raw %>%
+# Dependencies:
+source("package_en_developpement/R/pt_frequency.R")
+source("package_en_developpement/R/pt_time_floor.R")
+
+format_data <- function(data, date, count, time, phase, date_zero) {
+
+  # Data type checking
+  stopifnot(
+            class(data %>% select({{ date }}) %>% pull()) == "Date",
+            class(data %>% select({{ count }}) %>% pull()) == "numeric",
+            class(data %>% select({{ time }}) %>% pull()) == "numeric",
+            class({{ date_zero }}) == "Date"
+            )
+
+  # Format data
+  data <- data %>%
     transmute(
       date = {{ date }},
-      day = as.integer({{ date }} - as.Date({{ date_zero }})),
+      day = as.integer({{ date }} - {{ date_zero }}),
       count = {{ count }},
       time = {{ time }},
-      frequency = {{ count }}/{{ time }},
-      log10freq = log10(frequency)
+      timefloor = pt_time_floor({{ time }}),
+      frequency = pt_frequency({{ count }}, {{ time }}),
+      log10freq = log10(frequency),
+      phase = forcats::as_factor({{ phase }})
     )
-  
-  return(format_data)
+
+  # Ordering by date
+  data <- data %>% arrange(date)
+
+  return(data)
 }
