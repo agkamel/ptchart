9ac8ea8876a869ae45caf1cb9077daf3ce40bd93
agkamel <alexandre.gellen.kamel@gmail.com>
2022-04-22 23:48:19 -0400

    Rename function

--- R/pt_freq.R
+++ R/pt_freq.R
@@ -6,18 +6,18 @@
 #'
 #' @return A numeric vector.
 #'
-#' @examples #pt_frequency()
+#' @examples #pt_freq()
 #' @import dplyr
 #' @export
-pt_frequency <- function(count, time, rule = log10(2)) {
+pt_freq <- function(count, time, rule = log10(2)) {
   dplyr::case_when(
-    count == 0 ~ .frequency_if_zero_count(time, rule = rule),
+    count == 0 ~ .freq_if_zero_count(time, rule = rule),
     TRUE       ~ count / time
   )
 }
 
-.frequency_if_zero_count <- function(time, rule = log10(2)){
-  # Fonction helper .frequency_if_zero_count ####
+.freq_if_zero_count <- function(time, rule = log10(2)){
+  # Fonction helper .freq_if_zero_count ####
   #    Selon la suggestion de l'article de Neely
   antilog(log10(pt_time_floor(time)) - rule)
 }
--- tests/testthat/test-pt_freq.R
+++ tests/testthat/test-pt_freq.R
@@ -1,12 +1,12 @@
 test_that("function works properly", {
-  expect_equal(pt_frequency(count = c(1, 2, 1, 2, 5),
+  expect_equal(pt_freq(count = c(1, 2, 1, 2, 5),
                             time  = c(1, 1, 0.5, 0.5, 1)),
                expected = c(1, 2, 2, 4, 5)
                )
 })
 
 test_that("zero count", {
-  expect_equal(pt_frequency(count = c(0, 0, 0, 0, 0),
+  expect_equal(pt_freq(count = c(0, 0, 0, 0, 0),
                             time  = c(1, 2, 3, 4, 5)),
                expected = c(antilog(log10(1/1) - log10(2)),
                             antilog(log10(1/2) - log10(2)),
