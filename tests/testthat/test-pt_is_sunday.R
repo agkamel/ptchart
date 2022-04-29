b9eb7f46af13186e477a09118c16d30a56992ded
agkamel <alexandre.gellen.kamel@gmail.com>
2022-04-29 17:24:18 -0400

    Add test unit, expecting a sunday

--- tests/testthat/test-pt_is_sunday.R
+++ tests/testthat/test-pt_is_sunday.R
@@ -5,3 +5,9 @@ test_that("data param is of class Date", {
 test_that("data param is of class Date", {
   expect_error(pt_find_last_sunday(date = "2022-04-29"))
 })
+
+test_that("output is a sunday", {
+  expect_equal(pt_find_last_sunday(date = as.Date("2022-04-29")),
+               expected = as.Date("2022-04-24")
+  )
+})
