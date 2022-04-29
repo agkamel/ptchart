b418ba6643fbe1ff2f9c0b7f33477a4a3b7b745c
agkamel <alexandre.gellen.kamel@gmail.com>
2022-04-29 14:54:16 -0400

    Check if input is of class `date`

--- R/pt_is_sunday.R
+++ R/pt_is_sunday.R
@@ -8,5 +8,11 @@
 #'
 #' @examples # pt_is_sunday(date = as.Date(2022-04-29))
 pt_is_sunday <- function(date) {
+
+  # Check if input data are in the right format (if used outside of pt_format_data())
+  stopifnot(
+  "Argument `date` must be of class `Date`." = class({{ date_zero }}) == "Date"
+  )
+
   lubridate::wday(date, week_start = 1) == 7
 }
