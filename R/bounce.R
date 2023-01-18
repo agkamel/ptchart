5318303f5892af23afe3bc0896ebc5eb66f49351
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-18 10:28:44 -0500

    Add other bounce function into bounce file

--- R/bounce.R
+++ R/bounce.R
@@ -10,3 +10,21 @@ bounce_up <- function(day, log10_freq, raw = TRUE) {
 
 }
 
+bounce_down <- function(day, log10_freq, raw = TRUE) {
+  errors <- calculate_errors(day, log10_freq)
+  min_error <- antilog(min(errors))
+
+  if (raw == TRUE) {
+    min_error
+  } else {
+    convert_value(min_error)
+  }
+
+}
+
+bounce_total <- function(day, log10_freq) {
+  bounce_up <- bounce_up(day, log10_freq, raw = FALSE)
+  bounce_down <- bounce_down(day, log10_freq, raw = FALSE)
+
+  bounce_up * bounce_down
+}
