deb85072b74daf5d9b034ffbb062dfa3b159d0a1
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-18 10:27:53 -0500

    Fix antilog name; Add Oct value for times and div

--- R/celeration.R
+++ R/celeration.R
@@ -1,12 +1,12 @@
 celeration <- function(day, log10_freq, raw = TRUE) {
   b1 <- calculate_b1(day, log10_freq)
-  c_raw <- antilog10(b1)^7
+  c_raw <- antilog(b1)^7
   c_val <- convert_value(c_raw)
 
   if (c_raw == c_val) {
-    direction <- paste0("×", round(c_val, 2))
+    direction <- paste0("\327", round(c_val, 2))
   } else {
-    direction <- paste0("÷", round(c_val, 2))
+    direction <- paste0("\367", round(c_val, 2))
   }
 
   if (raw == TRUE) {
