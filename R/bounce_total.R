e3bef9f6a614e5c7dd587fed55af383ef27dba70
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-18 10:29:13 -0500

    Remove bounce_down and bounce_total

--- R/bounce_total.R
+++
@@ -1,6 +0,0 @@
-bounce_total <- function(day, log10_freq) {
-  bounce_up <- bounce_up(day, log10_freq, raw = FALSE)
-  bounce_down <- bounce_down(day, log10_freq, raw = FALSE)
-
-  bounce_up * bounce_down
-}
