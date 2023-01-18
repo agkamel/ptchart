df7fc834512ad6d8d48704615f34e045012fcfac
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-18 10:47:45 -0500

    Document file

--- R/bounce.R
+++ R/bounce.R
@@ -1,3 +1,13 @@
+#' Bounce
+#'
+#' @param day
+#' @param log10_freq
+#' @param raw
+#'
+#' @return A double.
+#' @export
+#'
+#' @examples
 bounce_up <- function(day, log10_freq, raw = TRUE) {
   errors <- calculate_errors(day, log10_freq)
   max_error <- antilog(max(errors))
@@ -7,9 +17,9 @@ bounce_up <- function(day, log10_freq, raw = TRUE) {
   } else {
     convert_value(max_error)
   }
-
 }
 
+#' @rdname add
 bounce_down <- function(day, log10_freq, raw = TRUE) {
   errors <- calculate_errors(day, log10_freq)
   min_error <- antilog(min(errors))
@@ -22,6 +32,7 @@ bounce_down <- function(day, log10_freq, raw = TRUE) {
 
 }
 
+#' @rdname add
 bounce_total <- function(day, log10_freq) {
   bounce_up <- bounce_up(day, log10_freq, raw = FALSE)
   bounce_down <- bounce_down(day, log10_freq, raw = FALSE)
