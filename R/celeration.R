90227af754420fa5dd530a06eba918cee96d69a2
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-19 15:53:16 -0500

    Document celeration

--- R/celeration.R
+++ R/celeration.R
@@ -1,3 +1,18 @@
+#' @title
+#' Celeration
+#'
+#' @description
+#' Calculate celeration.
+#'
+#' @param day Integer.
+#' @param log10_freq Double.
+#' @param raw Boolean.
+#'
+#' @return Double.
+#' @export
+#'
+#' @examples
+#' #TODO
 celeration <- function(day, log10_freq, raw = TRUE) {
   b1 <- calculate_b1(day, log10_freq)
   c_raw <- antilog(b1)^7
