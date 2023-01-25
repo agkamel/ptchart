1c043c616c95d348f6c10cb3ccc189d1c74e12a8
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-25 16:29:07 -0500

    Add stopifnot and test unit for antilog argument

--- tests/testthat/test-calculate_b1.R
+++ tests/testthat/test-calculate_b1.R
@@ -4,7 +4,14 @@ test_that("function raises an error if `day` is not integer", {
   expect_error(calculate_b1(day = TRUE, log10_freq = 0.30103))
 })
 
+
 test_that("function raises an error if `log10_freq` is not numeric", {
   expect_error(calculate_b1(day = 1, log10_freq = "0.30103"))
   expect_error(calculate_b1(day = 1, log10_freq = TRUE))
+})
+
+
+test_that("function raises an error if `antilog` is a logical TRUE or FALSE", {
+  expect_error(calculate_b1(day = 1, log10_freq = 0.30103, antilog = NA))
+  expect_error(calculate_b1(day = 1, log10_freq = TRUE, antilog = "TRUE"))
 })
