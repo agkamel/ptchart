1b31dd786616b2e1d799784f10823c527dd6f926
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-25 18:12:41 -0500

    Add stopifnot and unit tests for extractor functions

--- R/extractors.R
+++ R/extractors.R
@@ -1,44 +1,53 @@
 celeration <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- object[["c_table"]]
   ptobject
 }
 
 bounce <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- object[["b_table"]]
   ptobject
 }
 
 bounce_up <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- bounce(object)
   ptobject[c("phase", "b_up_raw", "b_up", "b_up_raw_err", "b_up_err")]
 }
 
 bounce_down <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- bounce(object)
   ptobject[c("phase", "b_down_raw", "b_down", "b_down_raw_err", "b_down_err")]
 }
 
 bounce_total <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- bounce(object)
   ptobject[c("phase","b_total", "b_total_err")]
 }
 
 jump <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- object[["j_table"]]
   ptobject
 }
 
 turn <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- object[["t_table"]]
   ptobject
 }
 
 describe <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- object[["desc"]]
   ptobject
 }
 
 terms <- function(object) {
+  stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   ptobject <- object[["terms"]]
   ptobject
 }
---
+++ tests/testthat/test-extractors.R
@@ -0,0 +1,35 @@
+test_that("object is of class ptstat", {
+  expect_error(celeration("x"))
+})
+
+test_that("object is of class ptstat", {
+  expect_error(bounce("x"))
+})
+
+test_that("object is of class ptstat", {
+  expect_error(bounce_up("x"))
+})
+
+test_that("object is of class ptstat", {
+  expect_error(bounce_down("x"))
+})
+
+test_that("object is of class ptstat", {
+  expect_error(bounce_total("x"))
+})
+
+test_that("object is of class ptstat", {
+  expect_error(jump("x"))
+})
+
+test_that("object is of class ptstat", {
+  expect_error(turn("x"))
+})
+
+test_that("object is of class ptstat", {
+  expect_error(terms("x"))
+})
+
+test_that("object is of class ptstat", {
+  expect_error(describe("x"))
+})
