9edc7dc9145c228de4cdd4d36f767001605d92c8
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-25 16:29:51 -0500

    Add stopifnot and test unit for antilog argument

--- R/calculate_errors.R
+++ R/calculate_errors.R
@@ -1,6 +1,7 @@
 calculate_errors <- function(day, log10_freq, antilog = FALSE, squared = FALSE){
   stopifnot("`day` must be integer" = is.integer(day))
   stopifnot("`log_10_freq` must be numeric" = is.numeric(log10_freq))
+  stopifnot("`antilog` must be a logical `TRUE` or `FALSE`" = is.logical(antilog) & antilog %in% c(TRUE, FALSE))
 
   predicted_values <- calculate_predicted_values(day, log10_freq)
 
--- R/calculate_predicted_values.R
+++ R/calculate_predicted_values.R
@@ -1,6 +1,7 @@
 calculate_predicted_values <- function(day, log10_freq, antilog = FALSE){
   stopifnot("`day` must be integer" = is.integer(day))
   stopifnot("`log_10_freq` must be numeric" = is.numeric(log10_freq))
+  stopifnot("`antilog` must be a logical `TRUE` or `FALSE`" = is.logical(antilog) & antilog %in% c(TRUE, FALSE))
 
   b0 <- calculate_b0(day, log10_freq)
   b1 <- calculate_b1(day, log10_freq)
