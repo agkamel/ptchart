29c4b8ba7d89fb437ec5e2f1ea27c83fa3f99937
agkamel <alexandre.gellen.kamel@gmail.com>
2023-01-26 20:43:28 -0500

    Add documentation for pt_data and pt_date_zero

---
+++ R/example_pt_data.R
@@ -0,0 +1,15 @@
+#' Example of dataset related to precision teaching
+#'
+#' Bla, bla, bla.
+#'
+#' @format A data frame with 20 rows and 7 variables:
+#' \describe{
+#'   \item{i}{index}
+#'   \item{date}{date of observation}
+#'   \item{jour}{day of observation}
+#'   \item{reponse}{number of response}
+#'   \item{minute}{number of minute}
+#'   \item{frequence}{frequency: number of response divided by number of minute}
+#'   \item{phase}{phase of observation}
+#' }
+"example_pt_data"
---
+++ R/example_pt_date_zero.R
@@ -0,0 +1,9 @@
+#' Date zero, to be used with `example_pt_data`
+#'
+#' Bla, bla, bla.
+#'
+#' @format A data frame with 20 rows and 7 variables:
+#' \describe{
+#'   \item{example_pt_date_zero}{date of the first sunday}
+#' }
+"example_pt_date_zero"
