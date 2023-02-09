2a0ec9b0edc3bc4f124066a48e7e52078c133082
agkamel <alexandre.gellen.kamel@gmail.com>
2023-02-09 08:42:04 -0500

    Add argument validation

--- R/convert_value.R
+++ R/convert_value.R
@@ -1,3 +1,10 @@
 convert_value <- function(x) {
+  if (!is.numeric(x)) {
+    stop("`x` must be numeric")
+  }
+  if (x <= 0) {
+    stop("`x` must be greater than 0")
+  }
+
   antilog(abs(log10(x)))
 }
