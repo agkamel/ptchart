2baad8713c357a91f45f84a59301e948e462736b
agkamel <alexandre.gellen.kamel@gmail.com>
2023-02-09 09:14:26 -0500

    Fix validation of values smaller of equal that zero to let pass NA; Update test unit

--- R/convert_value.R
+++ R/convert_value.R
@@ -1,9 +1,15 @@
 convert_value <- function(x) {
+
   if (!is.numeric(x)) {
     stop("`x` must be numeric")
   }
-  if (x <= 0) {
-    stop("`x` must be greater than 0")
+
+  for (i in x) {
+    if (is.na(i)) {
+      next
+    } else if (i <= 0) {
+      stop("Values in `x` must be greater than 0.")
+    }
   }
 
   antilog(abs(log10(x)))
--- tests/testthat/test-convert_value.R
+++ tests/testthat/test-convert_value.R
@@ -9,3 +9,11 @@ test_that("value smaller that 1 return its multiplicative inverse", {
 test_that("negative value raise an error", {
   expect_error(convert_value(-1))
 })
+
+test_that("NA_real_ return NA_real_", {
+  expect_equal(convert_value(NA_real_), NA_real_)
+})
+
+test_that("NA stay NA within numeric vector", {
+  expect_equal(convert_value(c(0.5, NA)), c(2, NA))
+})
