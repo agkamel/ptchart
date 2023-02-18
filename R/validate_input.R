c5b0237a687e9a0e0e91378bf024475b45bafcb5
agkamel <alexandre.gellen.kamel@gmail.com>
2023-02-18 15:34:36 -0500

    Add input validation for `freq` arg in `ptstat()`

--- R/validate_input.R
+++ R/validate_input.R
@@ -10,10 +10,28 @@ validate_arg_day <- function(data, day) {
   }
 
   if ((FALSE %in% (data[[day]] >= 0)) == TRUE) {
-    stop("`day must be greater or equal than 0`")
+    stop("`day` must be greater or equal than 0`")
   }
 
   if ((sum(data[[day]] %% 1) > 0) == TRUE) {
     stop("`day` must be a integer")
   }
 }
+
+validate_arg_freq <- function(data, freq) {
+  if (is.numeric(data[[freq]]) == FALSE) {
+    stop("`freq` must be numeric")
+  }
+
+  if ((FALSE %in% (data[[freq]] >= 0)) == TRUE) {
+    stop("`freq` must be greater or equal than 0`")
+  }
+
+  if ( ((Inf %in% (data[[freq]])) == TRUE) | ((-Inf %in% (data[[freq]])) == TRUE) ) {
+    stop("`freq` must not contain `Inf` or `-Inf` values")
+  }
+}
+
+
+
+
--- tests/testthat/test-validate_input.R
+++ tests/testthat/test-validate_input.R
@@ -5,6 +5,8 @@ test_that("Arg `data` in `ptstat()` throw an error if is not a data.frame", {
   expect_error(validate_arg_data(1L))
 })
 
+
+
 test_that("Arg `day` in `ptstat()` throw an error if is not numeric", {
   expect_error(validate_arg_day(data = data.frame(x = c("a", "b", "c")),
                                 day = "x"))
@@ -19,3 +21,18 @@ test_that("Arg `day` in `ptstat()` throw an error if is not an integer", {
 })
 
 
+
+test_that("Arg `freq` in `ptstat()` throw an error if is not numeric", {
+  expect_error(validate_arg_freq(data = data.frame(x = c("a", "b", "c")),
+                                freq = "x"))
+})
+test_that("Arg `freq` in `ptstat()` throw an error if is not greater than zero", {
+  expect_error(validate_arg_freq(data = data.frame(x = c(-1, 0, 1)),
+                                freq = "x"))
+})
+test_that("Arg `freq` in `ptstat()` throw an error if contains Inf or -Inf values", {
+  expect_error(validate_arg_freq(data = data.frame(x = c(0, 1, Inf)),
+                                 freq = "x"))
+  expect_error(validate_arg_freq(data = data.frame(x = c(0, 1, -Inf)),
+                                 freq = "x"))
+})
