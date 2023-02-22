0473ccc8796d923bd1b0ed63c5347c8632c13b77
agkamel <alexandre.gellen.kamel@gmail.com>
2023-02-22 15:55:35 -0500

    Update documentation for extractor functions

--- R/extractors.R
+++ R/extractors.R
@@ -1,31 +1,34 @@
-#' Extractor functions for ptstat object
+#' @title Extractor functions for ptstat object
 #'
 #' @param object An object of class `ptstat`
 #'
 #' @return A dataframe
-#' @export
 #'
 #' @examples
 #' #TODO
 
+#' @export
 #' @describeIn extractors Extract accuracy table
 accuracy <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   object[["a_table"]]
 }
 
+#' @export
 #' @describeIn extractors Extract celeration table
 celeration <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   object[["c_table"]]
 }
 
+#' @export
 #' @describeIn extractors Extract bounce table
 bounce <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   object[["b_table"]]
 }
 
+#' @export
 #' @describeIn extractors Extract only bounce up
 bounce_up <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
@@ -33,6 +36,7 @@ bounce_up <- function(object) {
   ptobject[c("phase", "b_up_raw", "b_up", "b_up_raw_err", "b_up_err")]
 }
 
+#' @export
 #' @describeIn extractors Extract only bounce down
 bounce_down <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
@@ -40,6 +44,7 @@ bounce_down <- function(object) {
   ptobject[c("phase", "b_down_raw", "b_down", "b_down_raw_err", "b_down_err")]
 }
 
+#' @export
 #' @describeIn extractors Extract only bounce total
 bounce_total <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
@@ -47,30 +52,35 @@ bounce_total <- function(object) {
   ptobject[c("phase","b_total", "b_total_err")]
 }
 
+#' @export
 #' @describeIn extractors Extract jump table
 jump <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   object[["j_table"]]
 }
 
+#' @export
 #' @describeIn extractors Extract turn table
 turn <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   object[["t_table"]]
 }
 
+#' @export
 #' @describeIn extractors Extract descriptive statistics table
 describe <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   object[["desc"]]
 }
 
+#' @export
 #' @describeIn extractors Extract regression terms table
 terms <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
   object[["terms"]]
 }
 
+#' @export
 #' @describeIn extractors Extract pttable table
 pttable <- function(object) {
   stopifnot("! `object` must be of class `ptstat`" = is_ptstat(object))
