ecb8f4e3ddbbb86d4f21dd8249f57ce32517f932
agkamel <alexandre.gellen.kamel@gmail.com>
2025-02-05 14:20:21 -0500

    Initial commit: Add data documentation of example_pt_data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Dataset example
+#'
+#' A fictional dataset for illustrating ptchart package functions.
+#'
+#' @format ## `example_pt_data`
+#' A data frame with 20 rows and 9 columns:
+#' \describe{
+#'   \item{i}{Observation index id.}
+#'   \item{date}{Date of observation in yyyy-mm-dd format.}
+#'   \item{jour}{Day of the observation.}
+#'   \item{minute}{Number of minutes.}
+#'   \item{reponse}{Count of target responses.}
+#'   \item{frequence}{Frequency of target responses.}
+#'   \item{reponse_nc}{Count of non-target responses.}
+#'   \item{frequence_nc}{Count of non-target responses.}
+#'   \item{phase}{Phase of intervention.}
+#' }
+"example_pt_data"
