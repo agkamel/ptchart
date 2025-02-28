9dd03bb0a8336e35bc731298cc955b06ce140812
agkamel <alexandre.gellen.kamel@gmail.com>
2025-02-28 15:44:46 -0500

    Add general tools script for working with precision teaching measures

---
+++ R/_development/other_tools.R
@@ -0,0 +1,75 @@
+angle_to_celeration <- function(x) {
+  NA
+}
+
+
+angle_to_celeration <- function(angle) {
+
+  tan(34*pi/180) * log10(2) * 7
+
+}
+
+
+d_tan <- function(angle) {
+  tan(angle * pi / 180)
+}
+
+d_atan <- function(ratio) {
+  atan(ratio) * 180 / pi
+}
+
+# En réalité
+d_tan(8.13)
+d_atan(0.1428553)
+1/7
+
+
+# 34 degre souhaité
+d_tan(34)    # 0.6745085
+d_tan(34)*7
+# Opp: 4.72156
+
+ratio_34_2 <- log10(4.72156) / log10(2)
+
+##
+d_tan(34) * 4.72156
+
+d_atan(ratio_34_2)
+
+
+
+angle_to_celeration()
+
+log10(1)
+log10(2)
+log10(5)
+log10(10)
+
+celeration_to_angle <- function(cel) {
+  #7/(log10(2)/tan(34*pi/180))
+
+  cel
+
+  7 * tan(34*pi/180) / log10(2)
+
+
+
+
+  seven_days <- 7
+
+
+  log_cel_value <- log10(2)
+
+
+
+
+  theta <- tan(34*pi/180)
+
+}
+
+
+
+
+tan(log10(2))^-1
+sin(log10(2))
+
