8bed614997b3222f4a9d9bd3b5ec849ec341addc
agkamel <alexandre.gellen.kamel@gmail.com>
2025-04-15 17:51:53 -0400

    Add tests for the new ptstat()

---
+++ tests/testthat/test-ptstat.R
@@ -0,0 +1,81 @@
+# df_test <- ptstat(example_pt_data,
+#        #date = date,
+#        day = jour,
+#        #freq = frequence,
+#        #freq_err = frequence_nc,
+#        count = reponse,
+#        count_err = reponse_nc,
+#        time = minute,
+#        phase = phase,
+#        )
+# df_test$main_df
+# df_test |> ptchart2()
+
+test_that("a S3 object of class ptstat is returned", {
+  expect_s3_class(ptstat(example_pt_data,
+                         day = jour,
+                         freq = frequence,
+                         phase = phase,
+                         #freq_err = frequence_nc
+                         ),
+                  "ptstat")
+})
+
+test_that("an error is raised because day, date, or date and date_zero is missing", {
+  expect_error(ptstat(example_pt_data,
+                         # day, date, or day and date missing
+                         freq = frequence,
+                         phase = phase,
+                         freq_err = frequence_nc
+                      ))
+})
+
+
+
+
+
+test_that("an error is raised because count, freq, count_err, or freq_err is missing.", {
+  expect_error(ptstat(example_pt_data,
+                      day = jour,
+                      #freq = frequence,
+                      #count = reponse,
+                      #count_err = reponse_nc,
+                      #freq_err = frequence_nc,
+                      #time = minute,
+                      phase = phase
+  ))
+
+  expect_error(ptstat(example_pt_data,
+                      day = jour,
+                      #freq = frequence,
+                      #count = reponse,
+                      #count_err = reponse_nc,
+                      #freq_err = frequence_nc,
+                      time = minute,
+                      phase = phase
+  ))
+})
+
+
+
+test_that("is_missing() and is_provided() works properly", {
+
+  expect_equal(is_missing(c(1, 2, 3)),
+               FALSE)
+  expect_equal(is_missing(c(NA, 2, 3)),
+               FALSE)
+  expect_equal(is_missing(c(NA, NA, NA)),
+               TRUE)
+
+  expect_equal(is_provided(c(1, 2, 3)),
+               TRUE)
+  expect_equal(is_provided(c(NA, 2, 3)),
+               TRUE)
+  expect_equal(is_provided(c(NA, NA, NA)),
+               FALSE)
+
+})
+
+
+# Test recoding algorithm
+
